81cc125745a33791a04d24493b20533a8679c125
Guilherme <guilhermegpereira26@hotmail.com>
2025-09-19 15:54:13 -0300

    add migrations

--- src/main/java/io/github/Guimaraes131/vroom/tag/TagController.java
+++ src/main/java/io/github/Guimaraes131/vroom/tag/TagController.java
@@ -1,6 +1,9 @@
 package io.github.Guimaraes131.vroom.tag;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TagController {
 
     @GetMapping()
-    public String index() {
+    public String index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        model.addAttribute("username", username);
+
         return "index";
     }
 }
--- src/main/resources/db/migration/V2__create_table_motorcycle.sql
+++ src/main/resources/db/migration/V2__create_table_motorcycle.sql
@@ -1,10 +1,3 @@
-CREATE TABLE tb_tag(
-    id UUID NOT NULL PRIMARY KEY,
-    coordinate VARCHAR(3) NOT NULL UNIQUE,
-    color VARCHAR(20),
-    is_available BOOLEAN NOT NULL
-);
-
 CREATE TABLE tb_motorcycle(
     id UUID NOT NULL PRIMARY KEY,
     license_plate VARCHAR(14) NOT NULL UNIQUE,
@@ -15,11 +8,4 @@ CREATE TABLE tb_motorcycle(
     tag_id UUID NOT NULL REFERENCES tb_tag(id),
     CONSTRAINT chk_model CHECK (model in ('MOTTUPOP', 'MOTTUSPORT', 'MOTTUE')),
     CONSTRAINT chk_problem CHECK (problem in ('MECHANICAL', 'ELECTRICAL', 'DOCUMENTATION', 'AESTHETIC', 'SAFETY', 'MULTIPLE', 'COMPLIANT'))
-);
-
-CREATE TABLE tb_user (
-    id UUID NOT NULL PRIMARY KEY,
-    login VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(300) NOT NULL,
-    roles VARCHAR[]
 );
